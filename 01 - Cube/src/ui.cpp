c1746873b5bb558d4f8d641626bfb34d62f6fee5
sersoid <serspgl@gmail.com>
2024-03-11 21:03:28 +0300

    Revert "Optimize renderWithTimeUpdate function"
    This reverts commit 23e40c58e83bd94072fcdea365b127c949925ff8.

--- 01 - Cube/src/ui.cpp
+++ 01 - Cube/src/ui.cpp
@@ -71,20 +71,15 @@ void animationDisableWidgets(const Ui::mainWindow& mainUI, const bool isDisable)
 
 std::function<long long(object&)> renderWithTimeUpdate(QLabel& resultLabel, QLabel& timeLabel, const std::function<std::map<std::string, std::vector<int>>(std::vector<std::vector<color>>&, object&, bool)>& renderFunc, const QSpinBox& resolutionSpinBox, const QCheckBox& coordSystemCheck) {
     return [&](object& obj) {
-        const int resolution = resolutionSpinBox.value();
-        std::vector image(resolution, std::vector<color>(resolution));
-
-#ifndef NDEBUG
         std::map<std::string, std::vector<int>> rc;
-        const long long elapsedTimeCalculation = doWithElapsedTime(renderFunc, rc, image, obj, coordSystemCheck.isChecked());
-        const long long elapsedTimeDebugPrint = doWithElapsedTime(printErrorDebug, static_cast<void*>(nullptr), rc);
-#else
-        const long long elapsedTimeCalculation = doWithElapsedTime(renderFunc, static_cast<void*>(nullptr), image, obj, coordSystemCheck.isChecked());
-#endif
+        const int resolution = resolutionSpinBox.value();
 
-        const long long elapsedTimeRender = doWithElapsedTime(updateQLabelByImage, static_cast<void*>(nullptr), resultLabel, image);
+        std::vector image(resolution, std::vector<color>(resolution));
+        const long long elapsedTimeCalculation = doWithElapsedTime(renderFunc, rc, image, obj, coordSystemCheck.isChecked());
+        const long long elapsedTimeRender = doWithElapsedTime(updateQLabelByImage, static_cast<void *>(nullptr), resultLabel, image);
 
 #ifndef NDEBUG
+        const long long elapsedTimeDebugPrint = doWithElapsedTime(printErrorDebug, static_cast<void *>(nullptr), rc);
         return updateTimeLabel(timeLabel, elapsedTimeCalculation, elapsedTimeRender, elapsedTimeDebugPrint);
 #else
         return updateTimeLabel(timeLabel, elapsedTimeCalculation, elapsedTimeRender);
